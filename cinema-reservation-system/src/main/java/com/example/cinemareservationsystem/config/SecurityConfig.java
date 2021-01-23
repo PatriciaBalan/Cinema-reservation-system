9b7608cc1285f772bb9d043b85ddc0a7f5027998
Patricia Balan <spatrissia91@yahoo.com>
2021-01-23 12:10:16 +0200

    final backend project auth

--- cinema-reservation-system/src/main/java/com/example/cinemareservationsystem/config/SecurityConfig.java
+++ cinema-reservation-system/src/main/java/com/example/cinemareservationsystem/config/SecurityConfig.java
@@ -1,29 +1,29 @@
-//package com.example.cinemareservationsystem.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.CorsConfigurationSource;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//
-//import java.util.Arrays;
-//
-//
-//@Configuration
-//    @EnableWebSecurity
-//    public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-////        @Override
-////        protected void configure(HttpSecurity http) throws Exception {
-////            http.authorizeRequests().anyRequest().permitAll();
-////            http.cors().and().csrf().disable();
-////        }
+package com.example.cinemareservationsystem.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+
+@Configuration
+    @EnableWebSecurity
+    public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().anyRequest().permitAll();
+//        http.cors().and().csrf().disable();
+//    }
+//}
 //
 ////        @Override
 ////        protected void configure(HttpSecurity http) throws Exception{
@@ -54,17 +54,17 @@
 //                    .roles("USER");
 //        }
 //
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            http.authorizeRequests()
-//                    .antMatchers("cinema/**").hasRole("ADMIN")
-//                    .anyRequest().authenticated()
-//            .and()
-//                    .httpBasic();
-//
-//            http.cors().and().csrf().disable();
-//        }
-//
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("cinema/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+
+        http.cors().and().csrf().disable();
+    }
+}
 //    @Bean
 //    public PasswordEncoder passwordEncoder() {
 //        return new BCryptPasswordEncoder();
